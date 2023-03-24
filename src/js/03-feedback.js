81e8a5ee0efad93533b76a77c50bc11de7aad214
Ernest-Kurish <kurish.ernest@gmail.com>
2023-03-24 23:42:20 +0900

    task - 3
    updated

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,34 +6,48 @@ const form = document.querySelector('.feedback-form');
 const emailInput = form.elements.email;
 const messageInput = form.elements.message;
 
-// Form state from local storage
+// Load the form state from local storage
 const savedState = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
 emailInput.value = savedState.email || '';
 messageInput.value = savedState.message || '';
 
-// Save the form state to storage
+// Save the form state to local storage
 const saveStateToStorage = throttle(() => {
-  const state = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  localStorage.setItem('feedback-form-state', JSON.stringify(state));
+const state = {
+email: emailInput.value,
+message: messageInput.value,
+};
+localStorage.setItem('feedback-form-state', JSON.stringify(state));
 }, 500);
 
 form.addEventListener('input', saveStateToStorage);
 
 form.addEventListener('submit', event => {
-  event.preventDefault();
+event.preventDefault();
 
-  // Log the form state to console
-  const currentState = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  console.log(currentState);
+// Check if all fields are filled in
+if (!validateForm()) {
+return;
+}
 
-  // Clear the form and local storage
-  emailInput.value = '';
-  messageInput.value = '';
-  localStorage.removeItem('feedback-form-state');
+// Log the current form state to console
+const currentState = {
+email: emailInput.value,
+message: messageInput.value,
+};
+console.log(currentState);
+
+// Clear the form and local storage
+emailInput.value = '';
+messageInput.value = '';
+localStorage.removeItem('feedback-form-state');
 });
+
+// Validate the form fields
+function validateForm() {
+if (emailInput.value.trim() === '' || messageInput.value.trim() === '') {
+alert('Please fill in all fields.');
+return false;
+}
+return true;
+}
